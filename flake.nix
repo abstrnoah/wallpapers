98cb3ab753f97e37549238aad9e31db9353f60b8
Noah D. Ortiz <abstrnoah@brumal.org>
2023-04-21 13:58:37 -0700

    init flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "a collection of wallpapers";
+
+  inputs.dotfiles.url = "github:abstrnoah/dotfiles/c801d944cd89bbf4b210891e1eb2c4e01761ecc3";
+
+  outputs =
+    inputs@{ dotfiles, ... }:
+    {
+      packages =
+        with dotfiles.lib.agnostic;
+        let
+          # Should support literally any system but yknow,,, flakes.
+          supported_systems = [ "x86_64-linux" ];
+          paths = list_dir ./wallpapers;
+          path_to_name =
+            path: builtins.replaceStrings [ "." ] [ "_" ] (baseNameOf path);
+        in
+        for_all supported_systems
+        (system:
+        builtins.listToAttrs
+        (map
+        (path: {
+          name = path_to_name path;
+          value = dotfiles.lib.${system}.store_file (./wallpapers + "/${path}") "";
+        })
+        paths));
+    };
+}
